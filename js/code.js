19091ed728e59e567498f776e40faf9828a3e0b5
Jasorza <41799764+Jasorza@users.noreply.github.com>
2019-03-14 10:13:37 -0500

    a

--- js/code.js
+++ js/code.js
@@ -1,72 +1,63 @@
 function revisar(elemento) {
-	if (elemento.value == '') {
-		elemento.className = 'error';
-	} else {
-		elemento.className = 'input';
-	}
-};
-
-function revisaNumero(elemento) {
-	if (elemento.value !== '') {
-		var data = elemento.value;
-		if (isNaN(data)) {
-			elemento.className = 'error';
-		} else {
-			elemento.className = 'input';
-		}
-	}
-};
-
-function revisaLongintud(elemento, min) {
-	if (elemento.value !== '') {
-		var data = elemento.value;
-		if (data.length < min) {
-			elemento.className = 'error';
-		} else {
-			elemento.className = 'input';
-		}
-	}
+    if (elemento.value == '') {
+        elemento.className = 'error';
+    } else {
+        elemento.className = 'input';
+    }
 };
 
 function revisaMail(elemento) {
-	if () {
-		var data = elemento.value;
-		var exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (!exp.test(data)) {
-			elemento.className = 'error';
-		} else {
-			elemento.className = 'input';
-		}
-	}
+    if (elemento.value !== '') {
+        var data = elemento.value;
+        var exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!exp.test(data)) {
+            elemento.className = 'error';
+        } else {
+            elemento.className = 'input';
+        }
+    }
+};
+
+function revisaPass(elemento) {
+    if (elemento.value !== '') {
+        var data = elemento.value;
+        regexp_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+        if (!regexp_password.test(data)) {
+            elemento.className = 'error';
+        } else {
+            elemento.className = 'input';
+        }
+    }
 };
 
 function validar() {
-	datosCorrectos = true;
-	error='';
-	if (document.getElementById('nombre').value == '') {
-		datosCorrectos = false;
-		error = 'El nombre esta vacio';
-	};
+    datosCorrectos = true;
+    error = '';
+    if (document.getElementById('nombre').value == '') {
+        datosCorrectos = false;
+        error = 'El nombre esta vacio';
+    };
 
-	if (document.getElementById('telefono').value == '') {
-		datosCorrectos = false;
-		error = 'El telefono esta vacio';
-	};
+    if (document.getElementById('telefono').value == '') {
+        datosCorrectos = false;
+        error = 'El telefono esta vacio';
+    };
 
-	if (isNaN(document.getElementById('telefono').value)) {
-		datosCorrectos = false;
-		error = 'El telefono solo debe tener numeros';
-	};
+    var regexp_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+    if (!regexp_password.test(document.getElementById('password').value)) {
+        datosCorrectos = false;
+        error = 'La contraseña no debe ser inferior a 8 caracteres debe tener: letras, números, caracteres especiales, mayúscula y mínuscula';
+    };
 
-	var exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if (!exp.test(document.getElementById('mail').value)) {
-		datosCorrectos = false;
-		error = 'El Correo es invalido';
-	};
+    var exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!exp.test(document.getElementById('mail').value)) {
+        datosCorrectos = false;
+        error = 'El Correo es invalido';
+    };
 
-	if (!datosCorrectos) {
-		alert('Hay errores en el formulario ' + error);
-	}
+    if (!datosCorrectos) {
+        alert('Hay errores en el formulario ' + error);
+    }
 
-	return datosCorrectos;
+    return datosCorrectos;
 };
